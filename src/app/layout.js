610382bbd778af34acbe4d46e374d54c82e8d147
Simanto Poddar <251956045+SIMANTO-PODDAR@users.noreply.github.com>
2026-05-15 15:15:17 +0600

    get all destinations

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Josefin_Sans } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const Josefin = Josefin_Sans({
   subsets: ["latin"],
 });
 
@@ -21,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${Josefin.className} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         {children}
